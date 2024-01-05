70f54040f44ed3c048be79cd5aad62ad74ea1220
anonimジ <125036778+anonim8559@users.noreply.github.com>
2024-01-05 14:12:03 +0100

    2 skrypt

--- users.php
+++ users.php
@@ -49,7 +49,7 @@ $conn->close();
         <h4>Podaj id użytkownika</h4>
         <form action="users.php" method="post">
       
-      <input type="number"/>
+      <input type="number" name="id" id="id"/>
 
       <br />
 
@@ -69,16 +69,35 @@ $database = "dane4";
 $conn = new mysqli($servername, $username, $password, $database);
 
 
-
-$sql = "SELECT id, imie, nazwisko, rok_urodzenia FROM osoby";
-$result = $conn->query($sql);
-
-
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 
+if (isset($_POST["id"])) {
+    $id = $_POST["id"];
+    $sql2 = "SELECT osoby.id, osoby.imie, osoby.nazwisko, osoby.zdjecie, osoby.rok_urodzenia, osoby.opis, hobby.nazwa FROM osoby, hobby WHERE osoby.Hobby_id=hobby.id AND osoby.id = $id";
+    $result2 = $conn->query($sql2);
+
+    if ($result2->num_rows > 0) {
+        $row2 = $result2->fetch_assoc();
+
+        echo '<h2>' . $row2["id"] . '. ' . $row2["imie"] . ' ' . $row2["nazwisko"] . '</h2>';
+
+        echo '<img src="' . $row2["zdjecie"] . '" alt="' . $id . '">';
+
+        echo '<p>Rok urodzenia: ' . $row2["rok_urodzenia"] . '</p>';
+        echo '<p>Opis: ' . $row2["opis"] . '</p>';
+        echo '<p>Hobby: ' . $row2["nazwa"] . '</p>';
+    } else {
+        echo "Brak danych dla wybranego ID";
+    }
+}
 
 $conn->close();
 ?>
+
+
     </div>
     
     </div>
